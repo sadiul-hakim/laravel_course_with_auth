0db837be195289d8d9aac8f9163ae561bdb9eb02
Sadiul Hakim <sadiulhakim@gmail.com>
2026-01-20 22:32:31 +0600

    Batch Jobs

--- routes/queues.php
+++ routes/queues.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Jobs\BatchJob1;
+use App\Jobs\BatchJob2;
+use App\Jobs\BatchJob3;
 use App\Jobs\FileWritterJob;
 use App\Jobs\GenerateInvoiceJob;
 use App\Jobs\InventoryReservationJob;
 use App\Jobs\UpdateOrderStatusJob;
+use Illuminate\Bus\Batch;
 use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/queues", function () {
@@ -23,3 +28,34 @@ Route::get("/chained-jobs", function () {
     ])
         ->dispatch();
 });
+
+Route::get("/batch-info/{batch}", function ($batch) {
+    return Bus::findBatch($batch);
+});
+Route::get("/batch-jobs", function () {
+
+    $batch = Bus::batch([
+        new BatchJob1(),
+        new BatchJob2(),
+        new BatchJob3()
+    ])
+        ->before(function (Batch $batch) {
+            Log::info($batch->progress());
+        })
+        ->progress(function (Batch $batch) {
+            Log::info($batch->progress());
+        })
+        ->then(function (Batch $batch) {})
+        ->catch(function (Batch $batch, Throwable $e) {
+            Log::error($e->getMessage());
+        })
+        ->finally(function (Batch $batch) {})
+        ->onConnection("database")
+        ->onQueue("default")
+        ->dispatch();
+
+    return $batch->id;
+});
+
+// Use chaining for dependent tasks and batch for independent tasks.
+// In Batching order does not matter but in chaining it matters.
